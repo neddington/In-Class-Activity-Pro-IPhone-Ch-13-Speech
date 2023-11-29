fba23b1f421959b54d119162a997c8d007fb1851
neddington <neddington@cascadia.edu>
2023-11-29 15:00:40 -0800

    Initial Commit

---
+++ In-Class Activity Pro IPhone Ch 13 Speech/In_Class_Activity_Pro_IPhone_Ch_13_SpeechApp.swift
@@ -0,0 +1,17 @@
+//
+//  In_Class_Activity_Pro_IPhone_Ch_13_SpeechApp.swift
+//  In-Class Activity Pro IPhone Ch 13 Speech
+//
+//  Created by Eddington, Nick on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct In_Class_Activity_Pro_IPhone_Ch_13_SpeechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
